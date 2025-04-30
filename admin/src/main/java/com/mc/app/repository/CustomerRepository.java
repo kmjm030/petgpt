f7aadd5d08bcb5097f7042216a8aa13b8f7d7551
octopusmon <kimjunseo111@naver.com>
2025-04-30 18:19:12 +0900

    25.04.30

--- admin/src/main/java/com/mc/app/repository/CustomerRepository.java
+++ admin/src/main/java/com/mc/app/repository/CustomerRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
-
+// d
 @Repository
 @Mapper
 public interface CustomerRepository extends MCRepository<Customer, String> {
